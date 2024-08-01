e3348eddc66440ada24d6c46bf75cadc5f9be4c1
Kei-dev-1213 <zz2003live@gmail.com>
2024-08-01 17:24:18 +0900

    yml変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { Form } from "./components/Form";
 import { RecordsList } from "./components/RecordsList";
-import { DB } from "./supabase/supabase";
+import { DB } from "./supabase";
 
 function App() {
 
