7bbb82fd8ee2767c888549f5e332006240c9bd71
Kei-dev-1213 <zz2003live@gmail.com>
2024-08-01 17:24:50 +0900

    yml変更

---
+++ src/supabase/index.js
@@ -0,0 +1,35 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
+// 取得
+const getAllRecords = async () => {
+    const records = await supabase.from("study-record").select("*");
+    return records;
+}
+
+// 登録
+const insertRecord = async ({ title, time }) => {
+    const { error } = await supabase
+        .from('study-record')
+        .insert({ title, time });
+    return error;
+}
+
+// 削除
+const deleteRecord = async (id) => {
+    const response = await supabase
+        .from('study-record')
+        .delete()
+        .eq('id', id);
+    return response;
+}
+
+export const DB = {
+    getAllRecords,
+    insertRecord,
+    deleteRecord
+}
